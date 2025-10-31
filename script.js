19abd37455e8a4be23a316c8923c2ded1f0c4160
yusufsefadogdu <yusuf.sefa43@icloud.com>
2025-10-31 20:03:43 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+ // Klavye tuşlarını dinle
+    document.addEventListener("keydown", function(event) {
+      if (event.key === "Enter") {
+        alert("GAY'LER BAŞKANI UYARISI GERİ DÖN!");
+      }
+    });
