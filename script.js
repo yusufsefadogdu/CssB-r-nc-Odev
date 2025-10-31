7cf91d06a849f9b48a08d0edc515b5ab81cf299b
yusufsefadogdu <yusuf.sefa43@icloud.com>
2025-10-31 20:15:24 +0300

    js dosyasında alert uyarısının yazısını değiştrifim

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
  // Klavye tuşlarını dinle
     document.addEventListener("keydown", function(event) {
       if (event.key === "Enter") {
-        alert("GAY'LER BAŞKANI UYARISI GERİ DÖN!");
+        alert("Hata Lütfen Geri Dön!!!");
       }
     });
